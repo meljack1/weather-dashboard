4ba2e4b9778a0d54b3ec2da1d77408926f501afd
Mel Jack <melissa_jack@hotmail.co.uk>
2021-07-05 01:33:49 +0100

    Dynamically update weather for today

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 const APIKey = "954a094b824cd8beb88a22ff29fc4fc0";
-const cityTitleEl = document.querySelector('[data-attr="location"]')
+const cityTitleEl = document.querySelector('[data-attr="location"]');
+const dateEl = document.querySelector('[data-attr="date"]');
+const temperatureTodayEl = document.querySelector('[data-attr="temperature-today"]');
+const windTodayEl = document.querySelector('[data-attr="wind-today"]');
+const humidityTodayEl = document.querySelector('[data-attr="humidity-today"]');
+const uvTodayEl = document.querySelector('[data-attr="uv-today"]');
+
+function getDate() {
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    const yyyy = today.getFullYear();
+
+    today = dd + '/' + mm + '/' + yyyy;
+    console.log(today);
+    return today;
+}
 
 // Gets city coordinates when you search
 function getCityCoordinates(city) {
@@ -23,12 +39,31 @@ function getWeatherData(latitude, longitude) {
   
     fetch(requestUrl)
       .then(function (response) {
-        return response.json();
+          return response.json();
       })
       .then(function (data) {
-        console.log(data);
+          const date = getDate();
+          const temp = data.current.temp;
+          const wind = data.current.wind_speed;
+          const humidity = data.current.humidity;
+          const uv = data.current.uvi;
+          updateCurrentForecast(date, temp, wind, humidity, uv)
+          console.log(data);
       });
 }
 
+function updateCurrentForecast(date, temp, wind, humidity, uv) {
+    dateEl.textContent = `(${date})`;
+    temperatureTodayEl.textContent = temp;
+    windTodayEl.textContent = wind;
+    humidityTodayEl.textContent = humidity;
+    uvTodayEl.textContent = uv;
+
+    console.log(date + temp + wind + humidity + uv)
+}
+
+function update5DayForecast() {
+
+}
  
-getCityCoordinates("Birmingham");
+getCityCoordinates("Birmingham, UK");
